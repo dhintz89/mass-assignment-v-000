3621dd4417342a63d46ef0014b08c7e5055bf905
Dan Hintz <dhintz@live.com>
2018-09-08 22:19:26 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,6 +4,6 @@ class Person
 :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize(attributes)
-    attributes.each {|att, val|
+    attributes.each {|att, val| self.send()
     
 end
